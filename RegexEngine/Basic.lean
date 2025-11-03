56e00939ea2f498f938d36c74019c1c94990f618
gawakawa <mashurakiryu@icloud.com>
2025-11-04 07:27:27 +0900

    🏷️ Define Regular Expressions

--- RegexEngine/Basic.lean
+++ RegexEngine/Basic.lean
@@ -1,8 +1,32 @@
+/--
+Definition of Regular Expressions
+
+r, s ::= ∅     empty set
+         ε     empty string
+         a     a ∈ Σ
+         r · s concatenation
+         r*    Kleene-closure
+         r + s logical or
+         r & s logical and
+         ¬ r   complement
+-/
 inductive Regex : Type where
+  | emptySet : Regex
   | epsilon : Regex
   | char : Char → Regex
   | concat : Regex → Regex → Regex
   | star : Regex → Regex
+  | or : Regex → Regex → Regex
+  | and : Regex → Regex → Regex
+  | compl : Regex → Regex
+
+notation "∅" => Regex.emptySet
+notation "ε" => Regex.epsilon
+infixl:70 " · " => Regex.concat
+infixl:60 " + " => Regex.or
+infixl:65 " & " => Regex.and
+postfix:max "*" => Regex.star
+prefix:75 "¬" => Regex.compl
 
 def nullable : Regex → Bool := sorry
 
